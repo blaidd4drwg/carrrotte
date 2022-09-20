af05a8d10f6a6ed4bcc4bcb6d9be7a0f66c48d15
Remo Roethlin <avelarius@gmail.com>
2022-09-21 00:30:27 +0200

    Integrated shinyCoreScan, large rewrite of code

---
+++ R/global.R
@@ -0,0 +1,11 @@
+gg2themes <- list(
+  "Standard ggplot2 theme" = ggplot2::theme_grey(),
+  "Black/white theme" = ggplot2::theme_bw(),
+  "Light theme" = ggplot2::theme_light(),
+  "Dark theme" = ggplot2::theme_dark(),
+  "Minimal theme" = ggplot2::theme_minimal(),
+  "Classic/R base theme" = ggplot2::theme_classic()
+)
+
+utils::globalVariables(".")
+utils::globalVariables("where")
